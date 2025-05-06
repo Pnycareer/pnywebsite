0659239810ef54d1c66db505f15115cf28c19854
rahilazhar12 <rahil.azhar1012@gmail.com>
2025-05-06 12:48:30 +0500

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,29 +1,29 @@
 import ClientComponent from "./(screens)/home/page";
 export default async function Home() {
-  // const metadata = await fetch(
-  //   "https://www.admin777.pny-trainings.com/api/metas/home",
-  //   {
-  //     cache: "no-cache",
-  //   }
-  // )
-  //   .then((response) => response.json())
-  //   .then((data) => ({
-  //     metatitle: data.metas[0].meta_title,
-  //     metadescription: data.metas[0].meta_description,
-  //   }))
-  //   .catch((error) => {
-  //     console.error("Error fetching metadata:", error);
-  //     return {
-  //       metatitle: "",
-  //       metadescription: "",
-  //     };
-  //   });
+  const metadata = await fetch(
+    "https://www.admin777.pny-trainings.com/api/metas/home",
+    {
+      cache: "no-cache",
+    }
+  )
+    .then((response) => response.json())
+    .then((data) => ({
+      metatitle: data.metas[0].meta_title,
+      metadescription: data.metas[0].meta_description,
+    }))
+    .catch((error) => {
+      console.error("Error fetching metadata:", error);
+      return {
+        metatitle: "",
+        metadescription: "",
+      };
+    });
 
-  const metadata = {
-    metatitle: "PNY Trainings Head Office – Arfa Karim Tower, Lahore",
-    metadescription:
-      "At Arfa Tower, we offer programs in Digital Marketing, Web Development, AI, Data Science, Graphic Design, E-Commerce, Cyber Security & Networking 0304-1111774",
-  };
+  // const metadata = {
+  //   metatitle: "PNY Trainings Head Office – Arfa Karim Tower, Lahore",
+  //   metadescription:
+  //     "At Arfa Tower, we offer programs in Digital Marketing, Web Development, AI, Data Science, Graphic Design, E-Commerce, Cyber Security & Networking 0304-1111774",
+  // };
 
   return (
     <>
